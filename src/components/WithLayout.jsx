790359559a9374c712a64372dc62b5bfdfe51d10
Rivki Zohar <rivkiii770@gmail.com>
2024-09-22 18:13:29 +0300

    fix: added link to home page in withLayout

--- src/components/WithLayout.jsx
+++ src/components/WithLayout.jsx
@@ -1,26 +1,29 @@
 import { Container, Box, Typography } from '@mui/material';
 import { AutoStoriesOutlined } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
 const withLayout = (WrappedComponent) => {
   const ComponentWithLayout = (props) => (
     <Container maxWidth='sm'>
       <Box>
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            padding: '0 8px',
-            fontWeight: 'bold',
-            py: 3,
-            mb: 2,
-          }}
-        >
-          <AutoStoriesOutlined sx={{ mr: 1 }} color='primary' />
-          <Typography variant='h6' noWrap component='div' color='primary'>
-            Book Tracker
-          </Typography>
-        </Box>
+        <Link to={'/'} style={{ textDecoration: 'none' }}>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: '0 8px',
+              fontWeight: 'bold',
+              py: 3,
+              mb: 2,
+            }}
+          >
+            <AutoStoriesOutlined sx={{ mr: 1 }} color='primary' />
+            <Typography variant='h6' noWrap component='div' color='primary'>
+              Book Tracker
+            </Typography>
+          </Box>
+        </Link>
         <WrappedComponent {...props} />
       </Box>
     </Container>
